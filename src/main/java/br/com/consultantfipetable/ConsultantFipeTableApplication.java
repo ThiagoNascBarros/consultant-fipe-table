ac7c53055fc8313e4e246519807e273054e85538
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-23 17:14:25 -0300

    Created project
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

---
+++ src/main/java/br/com/consultantfipetable/ConsultantFipeTableApplication.java
@@ -0,0 +1,13 @@
+package br.com.consultantfipetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultantFipeTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultantFipeTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/consultantfipetable/ConsultantFipeTableApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultantfipetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultantFipeTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
