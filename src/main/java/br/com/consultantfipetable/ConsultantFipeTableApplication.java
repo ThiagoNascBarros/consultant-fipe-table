5d7627edf75d6da51e2c95a2feb12d399d41c8fd
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-23 17:22:56 -0300

    feat: Implements CommandLineRunner in the main class.
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- src/main/java/br/com/consultantfipetable/ConsultantFipeTableApplication.java
+++ src/main/java/br/com/consultantfipetable/ConsultantFipeTableApplication.java
@@ -1,13 +1,19 @@
 package br.com.consultantfipetable;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsultantFipeTableApplication {
+public class ConsultantFipeTableApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsultantFipeTableApplication.class, args);
 	}
 
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
