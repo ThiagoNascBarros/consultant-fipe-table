be04c3fac1bec6e870c3eb99d403e2c3b66cf68f
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-12-29 23:17:57 -0300

    refactor: Remove unused getters and setters

--- src/main/java/br/com/consultantfipetable/domain/Data.java
+++ src/main/java/br/com/consultantfipetable/domain/Data.java
@@ -2,12 +2,14 @@ package br.com.consultantfipetable.domain;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.annotation.Nonnull;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Data(@JsonAlias("codigo") String code,
                    @JsonAlias("nome") String brand) {
 
     @Override
+    @Nonnull
     public String toString() {
         return "Code: " + code + " Brand: " + brand;
     }
--- src/main/java/br/com/consultantfipetable/domain/DataClass.java
+++ src/main/java/br/com/consultantfipetable/domain/DataClass.java
@@ -5,11 +5,6 @@ public class DataClass {
     private Integer code;
     private String brand;
 
-    public DataClass(Integer code, String brand) {
-        this.code = code;
-        this.brand = brand;
-    }
-
     public DataClass(String code, String brand) {
         this.code = Integer.parseInt(code);
         this.brand = brand;
@@ -17,18 +12,6 @@ public class DataClass {
 
     public Integer getCode() {
         return code;
-    }
-
-    public void setCode(Integer code) {
-        this.code = code;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
     }
 
     @Override
